4753c09991c5b165b1d851d22970fd4975d7ecac
Olga Tsuprik <tt.olia@mail.ru>
2021-02-06 04:02:47 +0300

    ADDED MenuProject

---
+++ MenuProject/Meal.swift
@@ -0,0 +1,29 @@
+//
+//  Meal.swift
+//  MenuProject
+//
+//  Created by Оля on 06.02.2021.
+//
+
+import UIKit
+
+class Meal {
+    // Properties
+    var name: String
+    var photo: UIImage?
+    var rating: Int
+    
+    // MARK: Initialization
+    init?(name: String, photo: UIImage?, rating: Int) {
+        guard !name.isEmpty else {
+            return nil
+        }
+        guard (rating >= 0) && (rating <= 5) else {
+            return nil
+        }
+        
+        self.name = name
+        self.photo = photo
+        self.rating = rating
+    }
+}
